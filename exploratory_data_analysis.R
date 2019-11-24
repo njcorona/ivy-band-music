be827da7899d7d2b3176fb8e5ca4295ac6757277
njcorona <43733460+njcorona@users.noreply.github.com>
2019-11-23 19:31:44 -0500

    Full Spotify data and full fall 2019 penn track info

--- exploratory_data_analysis.R
+++ exploratory_data_analysis.R
@@ -5,6 +5,51 @@
 library(tidyverse)
 library(tibble)
 
-cornell <- readRDS("cornell.RDS")
+cornell <- readRDS("cornell_full.RDS")
+penn <- readRDS("penn_full.RDS")
 
 boxplot(as.numeric(str_sub(cornell$track.album.release_date, start = 1, end = 4))) + title("Release years of Cornell Pep Band songs")
+
+boxplot(as.numeric(str_sub(penn$track.album.release_date, start = 1, end = 4))) + title("Release years of Penn Band songs")
+
+boxplot(penn$track.popularity) + title("Popularity of Penn Band songs")
+
+boxplot(penn$energy) + title("Energy of Penn Band songs")
+
+boxplot(penn$danceability) + title("Danceability of Penn Band songs")
+
+boxplot(penn$loudness) + title("Loudness of Penn Band songs")
+
+boxplot(penn$speechiness) + title("Speechiness of Penn Band songs")
+
+boxplot(penn$acousticness) + title("Acousticness of Penn Band songs")
+
+boxplot(penn$instrumentalness) + title("Instrumentalness of Penn Band songs")
+
+boxplot(penn$liveness) + title("Liveness of Penn Band songs")
+
+boxplot(penn$tempo) + title("Tempo of Penn Band songs")
+
+boxplot(penn$valence) + title("Valence of Penn Band songs")
+
+boxplot(cornell$track.popularity) + title("Popularity of Cornell Pep Band songs")
+
+boxplot(cornell$energy) + title("Energy of Cornell Pep Band songs")
+
+boxplot(cornell$danceability) + title("Danceability of Cornell Pep Band songs")
+
+boxplot(cornell$loudness) + title("Loudness of Cornell Pep Band songs")
+
+boxplot(cornell$speechiness) + title("Speechiness of Cornell Pep Band songs")
+
+boxplot(cornell$acousticness) + title("Acousticness of Cornell Pep Band songs")
+
+boxplot(cornell$instrumentalness) + title("Instrumentalness of Cornell Pep Band songs")
+
+boxplot(cornell$liveness) + title("Liveness of Cornell Pep Band songs")
+
+boxplot(cornell$tempo) + title("Tempo of Cornell Pep Band songs")
+
+boxplot(cornell$valence) + title("Valence of Cornell Pep Band songs")
+
+## This takes the purely older versions of songs.  If I want to get an accurate measure of popularity, I should get the most recent popular version of a song.
--- spotify_api_calls.R
+++ spotify_api_calls.R
@@ -2,12 +2,10 @@ library(spotifyr)
 library(tibble)
 library(stringr)
 
-SPOTIFY_CLIENT_ID <- ""
-SPOTIFY_CLIENT_SECRET <- ""
+Sys.setenv(SPOTIFY_CLIENT_ID = '')
+Sys.setenv(SPOTIFY_CLIENT_SECRET = '')
 
-
-access_token <- get_spotify_access_token(client_id = SPOTIFY_CLIENT_ID,
-                                         client_secret = SPOTIFY_CLIENT_SECRET)
+access_token <- get_spotify_access_token()
 
 # I looked at the Cornell Pep Band Spotify playlist put together here:  https://open.spotify.com/playlist/0a3fsqpDHy0a9Xo1Vc9BRH
 # I copied it into two playlists under my own account to allow me to use spotifyr to get the track info.
@@ -28,5 +26,21 @@ access_token <- get_spotify_access_token(client_id = SPOTIFY_CLIENT_ID,
 # saveRDS(cornell, "cornell.RDS")
 # 
 # cornell <- readRDS("cornell.RDS")
-
-boxplot(as.numeric(str_sub(cornell$track.album.release_date, start = 1, end = 4))) + title("Release years of Cornell Pep Band songs")
+# 
+# penn_raw <- get_playlist(playlist_id = "6FfEMbtwq4ptaJe1u0xmdx", authorization = access_token) 
+# penn <- as_tibble(penn_raw$tracks$items)
+# saveRDS(penn_raw, "penn_json.RDS")
+# saveRDS(penn, "penn_tibble.RDS")
+# 
+# playlist_username <- 'rkludtihkgic030glg9qaandf'
+# playlist_uris <- c('6FfEMbtwq4ptaJe1u0xmdx')
+# playlist_audio_features <- get_playlist_audio_features(playlist_username, playlist_uris)
+# saveRDS(playlist_audio_features, "penn_full.RDS")
+#
+# playlist_username <- 'rkludtihkgic030glg9qaandf'
+# playlist_uris1 <- c('3uhi2JenqbjkVN3XBABnSq')
+# playlist_uris2 <- c('6YhIW0wIoWjUXXMYTE3QIV')
+# playlist_audio_features1 <- get_playlist_audio_features(playlist_username, playlist_uris1)
+# playlist_audio_features2 <- get_playlist_audio_features(playlist_username, playlist_uris2)
+# playlist_audio_features <- rbind(playlist_audio_features1, playlist_audio_features2)
+# saveRDS(playlist_audio_features, "cornell_full.RDS")
